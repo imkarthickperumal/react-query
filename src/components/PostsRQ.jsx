6579f251cafd14d69bf6969ecbcd700af0236264
imkarthickperumal <karthickperumal.vp@gmail.com>
2025-03-07 15:49:46 +0530

    React Query | Stale Time

--- src/components/PostsRQ.jsx
+++ src/components/PostsRQ.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const PostsRQ = () => {
+  const { data, isLoading, isError, error, isFetching } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => {
+      return axios.get("http://localhost:4000/posts");
+    },
+    staleTime: 30000,
+  });
 
-    const { data, isLoading, isError, error } = useQuery({
-        queryKey: ["posts"],
-        queryFn: () => {
-            return axios.get("http://localhost:4000/posts")
-        }
-    })
+  console.log({ isLoading, isFetching });
 
-    if (isLoading) {
-        return <div>Page is loading...</div>
-    }
+  if (isLoading) {
+    return <div>Page is loading...</div>;
+  }
 
-    if (isError) {
-        return <div>{error.message}</div>
-    }
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
 
-
-    return (
-        <div className='post-list'>
-            {data?.data.map(post => (
-                <div className='post-item' key={post.id}>
-                    <h3 className='post-title'>{post.title}</h3>
-                    <p className='post-body'>{post.body}</p>
-                </div>
-            ))}
+  return (
+    <div className="post-list">
+      {data?.data.map((post) => (
+        <div className="post-item" key={post.id}>
+          <h3 className="post-title">{post.title}</h3>
+          <p className="post-body">{post.body}</p>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
-export default PostsRQ
+export default PostsRQ;
