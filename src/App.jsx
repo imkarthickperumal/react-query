ebf0abb5ea2528c4f60f0c4eb180e3e05c57d9c3
imkarthickperumal <karthickperumal.vp@gmail.com>
2025-03-11 12:54:33 +0530

    React Query | Infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import PostsTradetional from './components/PostsTradetional';
 import PostsRQ from './components/PostsRQ';
 import PostDetailsRQ from './components/PostDetailsRQ';
 import PaginatedQueries from './components/PaginatedQueries';
+import InfiniteQueries from './components/InfiniteQueries';
 
 
 function App() {
@@ -32,6 +33,7 @@ function App() {
           <Route exact path='/rq-posts' element={<PostsRQ />} />
           <Route exact path='/rq-posts/:postId' element={<PostDetailsRQ />} />
           <Route exact path='/paginated-fruits' element={<PaginatedQueries />} />
+          <Route exact path='/infinite-fruits' element={<InfiniteQueries />} />
         </Routes>
       </div>
     </BrowserRouter>
